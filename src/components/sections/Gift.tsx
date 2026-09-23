e52bfad754edec214991d4906efcb3537a7159fd
asepsulistiyono <138142982+asepsulistiyono@users.noreply.github.com>
2026-09-23 13:29:31 +0700

    Update import path for useWedding hook

--- src/components/sections/Gift.tsx
+++ src/components/sections/Gift.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { WEDDING } from "../../lib/wedding";
-import { useWedding } from "../../lib/WeddingContext";
+import { useWedding } from "../../hooks/useWedding";
 import { useReveal } from "../../hooks/useReveal";
 import { IconCheck, IconCopy, IconGift, IconPin } from "../Icons";
 import { SectionHead } from "../Decor";
